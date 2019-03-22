f0f04ac1cdf3a1e3744dbd0255d7ceb7e156e5f3
zhongjh <254191389@qq.com>
2019-03-22 15:32:20 +0800

    修复低版本因为svg引起的闪退的问题

--- app/src/main/java/com/zhongjh/cameraapp/MyApplication.java
+++ app/src/main/java/com/zhongjh/cameraapp/MyApplication.java
@@ -1,6 +1,8 @@
 package com.zhongjh.cameraapp;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.tencent.bugly.crashreport.CrashReport;
 
@@ -11,6 +13,12 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         CrashReport.initCrashReport(getApplicationContext(), "daa7c064ac", false);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
     }
 
 }
