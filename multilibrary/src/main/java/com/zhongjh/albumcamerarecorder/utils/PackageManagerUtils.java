8dfcc9887071fbe3bec380e932d435c98bc57d7a
zhongjh <254191389@qq.com>
2019-03-25 18:02:09 +0800

    1.增加因为闪光灯闪退的问题

---
+++ multilibrary/src/main/java/com/zhongjh/albumcamerarecorder/utils/PackageManagerUtils.java
@@ -0,0 +1,30 @@
+package com.zhongjh.albumcamerarecorder.utils;
+
+import android.content.pm.FeatureInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * 有关手机模块工具
+ * Created by zhongjh on 2019/3/25.
+ */
+public class PackageManagerUtils {
+
+    /**
+     * 判断是否支持闪光灯
+     * @param pm PackageManager
+     * @return 是否
+     */
+    public static boolean isSupportCameraLedFlash(PackageManager pm) {
+        if (pm != null) {
+            FeatureInfo[] features = pm.getSystemAvailableFeatures();
+            if (features != null) {
+                for (FeatureInfo f : features) {
+                    if (f != null && PackageManager.FEATURE_CAMERA_FLASH.equals(f.name));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
