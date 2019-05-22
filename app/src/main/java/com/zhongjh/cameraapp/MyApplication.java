353d876b090753432c34ac5d3a1d63342ca88535
zhongjh <254191389@qq.com>
2019-05-22 17:41:10 +0800

    ## 1.0.14  - 纯浏览图片功能增加了支持path方式。  - 这方面浏览功能考虑以后增加共享过渡动画(这个动画其实另外一个demo已经做了很久了，但是前段时间发现了内存泄漏，即使翻墙查询相关解决方法也解决不了，希望有研究过的大牛帮忙解答下：https://github.com/googlesamples/android-unsplash/issues/12)。  - 增加了内存泄漏检测，有相关内存泄漏的可以提供，会抓紧时间解决。

--- app/src/main/java/com/zhongjh/cameraapp/MyApplication.java
+++ app/src/main/java/com/zhongjh/cameraapp/MyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.squareup.leakcanary.LeakCanary;
 import com.tencent.bugly.crashreport.CrashReport;
 
 public class MyApplication extends Application {
@@ -18,7 +19,17 @@ public class MyApplication extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
+
+        // 腾讯提交bug
         MultiDex.install(base);
+
+        // 检测内存泄漏
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
     }
 
 }
