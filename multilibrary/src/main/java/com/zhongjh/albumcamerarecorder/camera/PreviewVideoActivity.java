d04ad962f856d5e63bb7761f85c9579c83c96a8e
zhongjh <254191389@qq.com>
2021-03-05 17:52:54 +0800

    1. 增加分段录制

--- multilibrary/src/main/java/com/zhongjh/albumcamerarecorder/camera/PreviewVideoActivity.java
+++ multilibrary/src/main/java/com/zhongjh/albumcamerarecorder/camera/PreviewVideoActivity.java
@@ -1,23 +1,38 @@
 package com.zhongjh.albumcamerarecorder.camera;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.zhongjh.albumcamerarecorder.R;
+import com.zhongjh.albumcamerarecorder.camera.widget.FullScreenVideoView;
 
 /**
  * 一个预览合成分段录制的视频
  */
 public class PreviewVideoActivity extends AppCompatActivity {
 
+    FullScreenVideoView mVVPreview;
+    ImageView mImgClose;
+    Button mBtnConfirm;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_preview_video);
     }
 
+    /**
+     * 初始化
+     */
+    private void initView() {
+        mVVPreview = findViewById(R.id.vvPreview);
+        mImgClose = findViewById(R.id.imgClose);
+        mBtnConfirm = findViewById(R.id.btnConfirm);
+    }
 
 
 }
