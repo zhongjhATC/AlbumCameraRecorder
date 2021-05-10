99a06d8ec1bee24becc3983d08c6bb37f3894fa1
zhongjh <254191389@qq.com>
2021-05-10 10:13:27 +0800

    增加内存泄漏检测

--- app/src/main/java/com/zhongjh/cameraapp/MyApplication.java
+++ app/src/main/java/com/zhongjh/cameraapp/MyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import androidx.multidex.MultiDex;
 
+import com.squareup.leakcanary.LeakCanary;
 import com.tencent.bugly.crashreport.CrashReport;
 
 /**
@@ -17,13 +18,10 @@ public class MyApplication extends Application {
         super.onCreate();
         CrashReport.initCrashReport(this, "daa7c064ac", false);
 
-//        // 检测内存泄漏
-//        if (LeakCanary.isInAnalyzerProcess(this)) {
-//            // This process is dedicated to LeakCanary for heap analysis.
-//            // You should not init your app in this process.
-//            return;
-//        }
-//        LeakCanary.install(this);
+        // 内存泄漏检测
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this);
+        }
     }
 
     @Override
