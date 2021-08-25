f6273a574be65f55288c073eafcb23d534db48a7
zhongjh <254191389@qq.com>
2021-08-25 17:58:42 +0800

    添加拍照后弹出角度toast,用于测试不同机器的旋转角度

--- multilibrary/src/main/java/com/zhongjh/albumcamerarecorder/camera/CameraLayout.java
+++ multilibrary/src/main/java/com/zhongjh/albumcamerarecorder/camera/CameraLayout.java
@@ -3,10 +3,10 @@ package com.zhongjh.albumcamerarecorder.camera;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -33,7 +33,6 @@ import com.otaliastudios.cameraview.CameraListener;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.PictureResult;
 import com.otaliastudios.cameraview.VideoResult;
-import com.otaliastudios.cameraview.controls.Facing;
 import com.otaliastudios.cameraview.controls.Flash;
 import com.zhongjh.albumcamerarecorder.R;
 import com.zhongjh.albumcamerarecorder.camera.common.Constants;
@@ -55,8 +54,8 @@ import com.zhongjh.albumcamerarecorder.settings.GlobalSpec;
 import com.zhongjh.albumcamerarecorder.utils.BitmapUtils;
 import com.zhongjh.albumcamerarecorder.utils.PackageManagerUtils;
 import com.zhongjh.albumcamerarecorder.utils.SelectableUtils;
-import com.zhongjh.albumcamerarecorder.widget.BaseOperationLayout;
 import com.zhongjh.albumcamerarecorder.widget.ChildClickableFrameLayout;
+import com.zhongjh.albumcamerarecorder.widget.BaseOperationLayout;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -126,6 +125,11 @@ public class CameraLayout extends RelativeLayout {
      * 当前界面的所有控件
      */
     public ViewHolder mViewHolder;
+
+    /**
+     * 默认图片
+     */
+    private Drawable mPlaceholder;
     /**
      * 拍照的图片-集合
      */
@@ -174,23 +178,22 @@ public class CameraLayout extends RelativeLayout {
     /**
      * 用于延迟隐藏的事件，如果不用延迟，会有短暂闪屏现象
      */
-    private final Handler mCameraViewGoneHandler = new Handler(Looper.getMainLooper());
+    private Handler mCameraViewGoneHandler = new Handler(Looper.getMainLooper());
     /**
      * 用于延迟显示的事件，如果不用延迟，会有短暂闪屏现象
      */
-    private final Handler mCameraViewVisibleHandler = new Handler(Looper.getMainLooper());
-    private final Runnable mCameraViewGoneRunnable = new Runnable() {
+    private Handler mCameraViewVisibleHandler = new Handler(Looper.getMainLooper());
+    private Runnable mCameraViewGoneRunnable = new Runnable() {
         @Override
         public void run() {
             // 隐藏cameraView
-            RelativeLayout.LayoutParams layoutParams = (LayoutParams) mViewHolder.cameraView.getLayoutParams();
+            ViewGroup.LayoutParams layoutParams = mViewHolder.cameraView.getLayoutParams();
             layoutParams.height = 1;
             layoutParams.width = 1;
-            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
             mViewHolder.cameraView.setLayoutParams(layoutParams);
         }
     };
-    private final Runnable mCameraViewVisibleRunnable = new Runnable() {
+    private Runnable mCameraViewVisibleRunnable = new Runnable() {
         @Override
         public void run() {
             // 隐藏播放视频
@@ -303,6 +306,11 @@ public class CameraLayout extends RelativeLayout {
         // 用于播放的视频file
         mVideoFile = mVideoMediaStoreCompat.getFilePath(1);
 
+        // 默认图片
+        TypedArray ta = mContext.getTheme().obtainStyledAttributes(
+                new int[]{R.attr.album_thumbnail_placeholder});
+        mPlaceholder = ta.getDrawable(0);
+
         // 设置图片路径
         if (mGlobalSpec.pictureStrategy != null) {
             // 如果设置了视频的文件夹路径，就使用它的
@@ -339,10 +347,7 @@ public class CameraLayout extends RelativeLayout {
         // 默认是快拍录制模式
         mViewHolder.pvLayout.getViewHolder().btnConfirm.setProgressMode(false);
 
-        // 初始化cameraView,接口引用到外面
-        if (mCameraSpec.onCameraViewListener != null) {
-            mCameraSpec.onCameraViewListener.onInitListener(mViewHolder.cameraView);
-        }
+        // 初始化cameraView
 
         setFlashLamp(); // 设置闪光灯模式
         mViewHolder.imgSwitch.setImageResource(mCameraSpec.imageSwitch);
@@ -674,36 +679,13 @@ public class CameraLayout extends RelativeLayout {
 
             @Override
             public void onPictureTaken(@NonNull PictureResult result) {
+                Toast.makeText(mContext, "角度" + getNaturalOrientation(result.getData()), Toast.LENGTH_SHORT).show();
                 result.toBitmap(bitmap -> {
-                    boolean isFront = false;
-                    if (mViewHolder.cameraView.getFacing().equals(Facing.FRONT)) {
-                        // 如果是前面
-                        isFront = true;
-                    }
-                    android.hardware.camera2.CameraManager manager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
-                    try {
-                        for (int i = 0; i < manager.getCameraIdList().length; i++) {
-                            CameraCharacteristics characteristics = manager.getCameraCharacteristics(manager.getCameraIdList()[i]);
-                            Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
-                            if (isFront && facing == CameraCharacteristics.LENS_FACING_FRONT) {
-                                // 目前打开摄像头的是前置，获取角度
-                                int sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
-                                Toast.makeText(mContext, "角度：" + sensorOrientation, Toast.LENGTH_SHORT).show();
-                            } else if (!isFront && facing == CameraCharacteristics.LENS_FACING_BACK) {
-                                {
-                                    // 目前打开摄像头的是后置，获取角度
-                                    int sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
-                                    Toast.makeText(mContext, "角度：" + sensorOrientation, Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        }
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
                     // 显示图片
                     showPicture(bitmap);
                     // 恢复点击
                     mViewHolder.rlMain.setChildClickable(true);
+
                 });
                 super.onPictureTaken(result);
             }
@@ -1185,10 +1167,9 @@ public class CameraLayout extends RelativeLayout {
      */
     private void setCameraViewVisible() {
         // 录制显示就隐藏播放
-        RelativeLayout.LayoutParams layoutParams = (LayoutParams) mViewHolder.cameraView.getLayoutParams();
+        ViewGroup.LayoutParams layoutParams = mViewHolder.cameraView.getLayoutParams();
         layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
         layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
-        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
         mViewHolder.cameraView.setLayoutParams(layoutParams);
         mCameraViewVisibleHandler.postDelayed(mCameraViewVisibleRunnable, 300);
     }
@@ -1302,6 +1283,119 @@ public class CameraLayout extends RelativeLayout {
         } else {
             mViewHolder.imgSwitch.setVisibility(viewVisibility);
         }
+    }
+
+    /**
+     * 从底层返回的数据拿到对应的图片的角度，有些手机hal层会对手机拍出来的照片作相应的旋转，有些手机不会(比如三星手机)
+     *
+     * @param jpeg
+     * @return
+     */
+    public static int getNaturalOrientation(byte[] jpeg) {
+        if (jpeg == null) {
+            return 0;
+        }
+
+        int offset = 0;
+        int length = 0;
+
+        // ISO/IEC 10918-1:1993(E)
+        while (offset + 3 < jpeg.length && (jpeg[offset++] & 0xFF) == 0xFF) {
+            int marker = jpeg[offset] & 0xFF;
+
+            // Check if the marker is a padding.
+            if (marker == 0xFF) {
+                continue;
+            }
+            offset++;
+
+            // Check if the marker is SOI or TEM.
+            if (marker == 0xD8 || marker == 0x01) {
+                continue;
+            }
+            // Check if the marker is EOI or SOS.
+            if (marker == 0xD9 || marker == 0xDA) {
+                break;
+            }
+
+            // Get the length and check if it is reasonable.
+            length = pack(jpeg, offset, 2, false);
+            if (length < 2 || offset + length > jpeg.length) {
+                return 0;
+            }
+
+            // Break if the marker is EXIF in APP1.
+            if (marker == 0xE1 && length >= 8 &&
+                    pack(jpeg, offset + 2, 4, false) == 0x45786966 &&
+                    pack(jpeg, offset + 6, 2, false) == 0) {
+                offset += 8;
+                length -= 8;
+                break;
+            }
+
+            // Skip other markers.
+            offset += length;
+            length = 0;
+        }
+
+        // JEITA CP-3451 Exif Version 2.2
+        if (length > 8) {
+            // Identify the byte order.
+            int tag = pack(jpeg, offset, 4, false);
+            if (tag != 0x49492A00 && tag != 0x4D4D002A) {
+                return 0;
+            }
+            boolean littleEndian = (tag == 0x49492A00);
+
+            // Get the offset and check if it is reasonable.
+            int count = pack(jpeg, offset + 4, 4, littleEndian) + 2;
+            if (count < 10 || count > length) {
+                return 0;
+            }
+            offset += count;
+            length -= count;
+
+            // Get the count and go through all the elements.
+            count = pack(jpeg, offset - 2, 2, littleEndian);
+            while (count-- > 0 && length >= 12) {
+                // Get the tag and check if it is orientation.
+                tag = pack(jpeg, offset, 2, littleEndian);
+                if (tag == 0x0112) {
+                    // We do not really care about type and count, do we?
+                    int orientation = pack(jpeg, offset + 8, 2, littleEndian);
+                    switch (orientation) {
+                        case 1:
+                            return 0;
+                        case 3:
+                            return 180;
+                        case 6:
+                            return 90;
+                        case 8:
+                            return 270;
+                    }
+                    return 0;
+                }
+                offset += 12;
+                length -= 12;
+            }
+        }
+        return 0;
+    }
+
+    private static int pack(byte[] bytes, int offset, int length,
+                            boolean littleEndian) {
+        int step = 1;
+        if (littleEndian) {
+            offset += length - 1;
+            step = -1;
+        }
+
+        int value = 0;
+        while (length-- > 0) {
+            value = (value << 8) | (bytes[offset] & 0xFF);
+            offset += step;
+        }
+        return value;
     }
 
     /**
