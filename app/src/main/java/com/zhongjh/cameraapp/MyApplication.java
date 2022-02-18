e6b0c3655f340e47bb306489e12e3b2bf7b7ba25
zhongjh <254191389@qq.com>
2022-02-18 15:46:55 +0800

    增加新版的内存泄漏检测

--- app/src/main/java/com/zhongjh/cameraapp/MyApplication.java
+++ app/src/main/java/com/zhongjh/cameraapp/MyApplication.java
@@ -3,8 +3,6 @@ package com.zhongjh.cameraapp;
 import android.app.Application;
 import android.content.Context;
 import androidx.multidex.MultiDex;
-
-import com.squareup.leakcanary.LeakCanary;
 import com.tencent.bugly.crashreport.CrashReport;
 
 /**
