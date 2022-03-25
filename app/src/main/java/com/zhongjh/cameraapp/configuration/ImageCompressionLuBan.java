436cb73e20687a1ff20020aa30f88f6eab6effa4
zhongjh <254191389@qq.com>
2022-03-25 09:40:04 +0800

    修改demo增加 Compressor 的一个压缩选择

--- app/src/main/java/com/zhongjh/cameraapp/configuration/ImageCompressionLuBan.java
+++ app/src/main/java/com/zhongjh/cameraapp/configuration/ImageCompressionLuBan.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.io.IOException;
 
-import top.zibin.luban.Luban;
+import id.zelory.compressor.Compressor;
 
 /**
  * luban压缩
@@ -21,7 +21,10 @@ public class ImageCompressionLuBan implements ImageCompressionInterface {
 
     @Override
     public File compressionFile(@NotNull Context context, @NotNull File file) throws IOException {
-        return Luban.with(context).load(file).get().get(0);
+        // 这是luban压缩
+//        return Luban.with(context).load(file).get().get(0);
+        // 这是Compressor压缩，选择你平常使用的压缩
+        return new Compressor(context).compressToFile(file);
     }
 
 }
