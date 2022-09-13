49ce60441a55031c850b5bffe90bf73c010eb4d0
zhongjh <254191389@qq.com>
2022-09-13 16:58:37 +0800

    完善 MediaLoader

--- multilibrary/src/main/java/com/zhongjh/albumcamerarecorder/album/loader/MediaLoader.java
+++ multilibrary/src/main/java/com/zhongjh/albumcamerarecorder/album/loader/MediaLoader.java
@@ -24,6 +24,7 @@ public class MediaLoader {
      * 来自于多媒体的数据源标记
      */
     private static final Uri QUERY_URI = MediaStore.Files.getContentUri("external");
+    private static final String GROUP_BY_BUCKET_Id = " GROUP BY (bucket_id";
     private static final String COLUMN_COUNT = "count";
     private static final String COLUMN_BUCKET_ID = "bucket_id";
     private static final String COLUMN_BUCKET_DISPLAY_NAME = "bucket_display_name";
@@ -87,7 +88,7 @@ public class MediaLoader {
             "COUNT(*) AS " + COLUMN_COUNT};
 
     /**
-     * 根据配置而定制的查询条件
+     * 构造 根据配置而定制的查询条件
      *
      * @return 查询条件字符串
      */
@@ -95,39 +96,77 @@ public class MediaLoader {
         String fileSizeCondition = getFileSizeCondition();
         if (AlbumSpec.INSTANCE.onlyShowImages()) {
             // 只查询图片
-            return getSelectionArgsForImageMediaCondition(queryMimeCondition, fileSizeCondition);
+            return getSelectionByImageCondition(fileSizeCondition);
         } else if (AlbumSpec.INSTANCE.onlyShowVideos()) {
             // 只查询视频
-            return getSelectionArgsForVideoOrAudioMediaCondition(queryMimeCondition, fileSizeCondition);
+            return getSelectionByVideoCondition(fileSizeCondition);
         } else {
             // 查询所有
-            return getSelectionArgsForAllMediaCondition(getDurationCondition(), fileSizeCondition, queryMimeCondition);
+            return getSelectionArgsForAllMediaCondition(getDurationCondition(), fileSizeCondition);
         }
     }
 
     /**
-     * 获取多媒体最大值查询条件的构造字符串
+     * 构造多媒体最大值查询条件的字符串
      *
      * @return 多媒体最大值查询条件的构造字符串
      */
     private String getFileSizeCondition() {
         // 获取文件最大值
-        long maxFileSize = CameraSpec.INSTANCE.getMinDuration() == 0 ? Long.MAX_VALUE : AlbumSpec.INSTANCE.getOriginalMaxSize();
+        long maxFileSize = AlbumSpec.INSTANCE.getOriginalMaxSize() == 0 ? Long.MAX_VALUE : AlbumSpec.INSTANCE.getOriginalMaxSize();
         return String.format(Locale.CHINA, MediaStore.MediaColumns.SIZE + " > 0 and " + MediaStore.MediaColumns.SIZE + " <= %d",
                 maxFileSize);
     }
 
     /**
-     * Get video (maximum or minimum time)
+     * 构造视频的时长条件字符串
      *
-     * @return
+     * @return 视频的时长条件字符串
      */
     private String getDurationCondition() {
-        long maxS = config.videoMaxSecond == 0 ? Long.MAX_VALUE : config.videoMaxSecond;
-        return String.format(Locale.CHINA, "%d <%s " + MediaStore.MediaColumns.DURATION + " and " + MediaStore.MediaColumns.DURATION + " <= %d",
-                Math.max((long) 0, config.videoMinSecond),
-                Math.max((long) 0, config.videoMinSecond) == 0 ? "" : "=",
-                maxS);
+        long maxS = CameraSpec.INSTANCE.getMaxDuration() == 0 ? Long.MAX_VALUE : CameraSpec.INSTANCE.getMaxDuration();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return String.format(Locale.CHINA, "%d <%s " + MediaStore.MediaColumns.DURATION + " and " + MediaStore.MediaColumns.DURATION + " <= %d",
+                    Math.max((long) 0, CameraSpec.INSTANCE.getMinDuration()),
+                    Math.max((long) 0, CameraSpec.INSTANCE.getMinDuration()) == 0 ? "" : "=",
+                    maxS);
+        } else {
+            return String.format(Locale.CHINA, "%d <%s duration and duration <= %d",
+                    Math.max((long) 0, CameraSpec.INSTANCE.getMinDuration()),
+                    Math.max((long) 0, CameraSpec.INSTANCE.getMinDuration()) == 0 ? "" : "=",
+                    maxS);
+        }
+    }
+
+    /**
+     * 构造查询条件字符串 - 图片
+     *
+     * @param fileSizeCondition 多媒体最大值查询条件字符串
+     * @return 条件字符串
+     */
+    private static String getSelectionByImageCondition(String fileSizeCondition) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return stringBuilder.append(MediaStore.Files.FileColumns.MEDIA_TYPE).append("=?").append(fileSizeCondition).toString();
+        } else {
+            // ContentResolver().query() 会生成 "WHERE (1 = 1) AND (xxxx)",所以 GROUP_BY_BUCKET_Id 要自带一个半括号"("
+            return stringBuilder.append("(").append(MediaStore.Files.FileColumns.MEDIA_TYPE).append("=?").append(") AND ").append(fileSizeCondition).append(")").append(GROUP_BY_BUCKET_Id).toString();
+        }
+    }
+
+    /**
+     * 构造查询条件字符串 - 视频
+     *
+     * @param fileSizeCondition 多媒体最大值查询条件字符串
+     * @return 条件字符串
+     */
+    private static String getSelectionByVideoCondition(String fileSizeCondition) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return stringBuilder.append(MediaStore.Files.FileColumns.MEDIA_TYPE).append("=?").append(" AND ").append(fileSizeCondition).toString();
+        } else {
+            return stringBuilder.append("(").append(MediaStore.Files.FileColumns.MEDIA_TYPE).append("=?").append(") AND ").append(fileSizeCondition).append(")").append(GROUP_BY_BUCKET_Id).toString();
+        }
     }
 
 }
