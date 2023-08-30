2de2a78b23fb002baa3496f7d476c56b82090cb7
zhongjh <20249019>
2023-08-30 14:24:28 +0800

    1. TextView添加tools使得xml可看

---
+++ multilibrary/src/main/java/com/zhongjh/albumcamerarecorder/widget/SharedAnimationView.kt
@@ -0,0 +1,19 @@
+package com.zhongjh.albumcamerarecorder.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+/**
+ * 一个共享动画的View
+ * 容器可以添加任何View
+ */
+class SharedAnimationView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : FrameLayout(
+    context, attrs, defStyleAttr
+) {
+    
+}
