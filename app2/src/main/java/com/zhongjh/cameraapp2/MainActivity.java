3d93818944b58cfbbc2d3b896908d71de652867f
zhongjh <20249019>
2023-11-21 11:20:09 +0800

    1. 添加app2演示demo

---
+++ app2/src/main/java/com/zhongjh/cameraapp2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zhongjh.cameraapp2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
